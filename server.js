b050425202f3f4cd4039b49c1b89d37b4922aa0c
jojimjohn <philijoji@gmail.com>
2025-10-07 12:50:04 +0800

    server config

--- server.js
+++ server.js
@@ -76,8 +76,7 @@ const corsOptions = {
     const allowedOrigins = [
       'http://localhost:3000',
       'http://127.0.0.1:3000',
-      'http://localhost:5173',
-      'http://127.0.0.1:5173'
+      'http://173.212.223.137:3000'
     ];
     
     if (process.env.NODE_ENV === 'production') {
